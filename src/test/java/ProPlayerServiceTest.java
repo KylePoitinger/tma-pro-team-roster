91bc96f1b4d56801fc15f4b45f2c83b0c6fb692f
Kyle Poitinger <kyle.poitinger@gmail.com>
2021-05-16 14:51:38 -0400

    maven issues for junit, but i tried :D

--- src/test/java/ProPlayerServiceTest.java
+++ src/test/java/ProPlayerServiceTest.java
@@ -1,51 +1,88 @@
-//package test.java;
-//
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(ProPlayerService.class)
-//public class ProPlayerServiceTest {
+package test.java;
 
-//	private ProPlayerService proPlayerService;
-//	private ProPlayerRepo proPlayerRepo;
-//
-//	@Mock
-//	private ProPlayerEntity proPlayer;
-//
-//	@Before
-//	public void setupMock() {
-//		proPlayerService = new ProPlayerService(proPlayerRepo);
-//		MockitoAnnotations.initMocks(this);
-//	}
-//
-//	@Test
-//	public void testMockCreation() {
-//		assertNotNull(proPlayer);
-//	}
-//
-//	@Test
-//	public void get_pro_player_test() {
-//		assertTrue(ProPlayerService.getProPlayer(1).name == "Kyle");
-//	}
-//
-//	@Test
-//	public void create_pro_player_test() {
-//		assertTrue(ProPlayerService.getProPlayer(proPlayer.playerId) == null);
-//		ProPlayerService.createProPlayer(proPlayer);
-//		assertTrue(ProPlayerService.getProPlayer(proPlayer.playerId) != null);
-//	}
-//
-//	@Test
-//	public void update_pro_player_test() {
-//		proPlayer.salary = 999999;
-//		assertTrue(ProPlayerService.getProPlayer(proPlayer.playerId).salary != 999999);
-//		ProPlayerService.updateProPlayer(proPlayer.playerId, proPlayer);
-//		assertTrue(ProPlayerService.getProPlayer(proPlayer.playerId).salary == 999999);
-//	}
-//
-//	@Test
-//	public void delete_pro_player_test() {
-//		assertTrue(ProPlayerService.getProPlayer(proPlayer.playerId) != null);
-//		ProPlayerService.deleteProPlayer(proPlayer.playerId);
-//		assertTrue(ProPlayerService.getProPlayer(proPlayer.playerId) == null);
-//	}
-//
-//}
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.entity.ProPlayerEntity;
+import main.java.service.ProPlayerService;
+
+public class ProPlayerServiceTest {
+
+	@Before
+	public void setUp() {
+		ProPlayerEntity player1 = new ProPlayerEntity();
+		player1.playerId = 1;
+		player1.name = "Kyle";
+		player1.position = "Forward";
+		player1.teamName = "Hornets";
+		player1.age = 24;
+		player1.height = 175.26;
+		player1.weight = 176.4;
+		player1.college = "UNCC";
+		player1.salary = 100000.00;
+		ProPlayerEntity player2 = new ProPlayerEntity();
+		player2.playerId = 2;
+		player2.name = "Max";
+		player2.position = "Defender";
+		player2.teamName = "Apps";
+		player2.age = 24;
+		player2.height = 173.26;
+		player2.weight = 187.4;
+		player2.college = "APP";
+		player2.salary = 100540.00;
+		ProPlayerEntity player3 = new ProPlayerEntity();
+		player3.playerId = 3;
+		player3.name = "Kat";
+		player3.position = "Midfield";
+		player3.teamName = "Switch";
+		player3.age = 25;
+		player3.height = 173.26;
+		player3.weight = 187.4;
+		player3.college = "ASHE";
+		player3.salary = 120547.00;
+	}
+
+	@Test
+	public void get_pro_player_test() {
+		assertTrue(ProPlayerService.getProPlayer(1).name == "Kyle");
+	}
+
+	@Test
+	public void create_pro_player_test() {
+		ProPlayerEntity player4 = new ProPlayerEntity();
+		player4.playerId = 4;
+		player4.name = "Rush";
+		player4.position = "Goalie";
+		player4.teamName = "Lilo";
+		player4.age = 85;
+		player4.height = 73.26;
+		player4.weight = 87.4;
+		player4.college = "ASHE";
+		player4.salary = 120547.00;
+		assertTrue(ProPlayerService.getProPlayer(4) == null);
+		ProPlayerService.createProPlayer(player4);
+		assertTrue(ProPlayerService.getProPlayer(4) != null);
+	}
+
+	@Test
+	public void update_pro_player_test() {
+		ProPlayerEntity player2 = new ProPlayerEntity();
+		player2.playerId = 2;
+		player2.salary = 1;
+		assertTrue(ProPlayerService.getProPlayer(player2.playerId).salary != 999999);
+		ProPlayerService.updateProPlayer(player2.playerId, player2);
+		assertTrue(ProPlayerService.getProPlayer(player2.playerId).salary == 1);
+	}
+
+	@Test
+	public void delete_pro_player_test() {
+		ProPlayerEntity player4 = new ProPlayerEntity();
+		player4.playerId = 4;
+		assertTrue(ProPlayerService.getProPlayer(player4.playerId) != null);
+		ProPlayerService.deleteProPlayer(player4.playerId);
+		assertTrue(ProPlayerService.getProPlayer(player4.playerId) == null);
+	}
+
+}
--- src/test/java/ProTeamServiceTest.java
+++ src/test/java/ProTeamServiceTest.java
@@ -1,69 +1,74 @@
-//package test.java;
-//
-//import static org.junit.Assert.assertTrue;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import main.java.entity.ProTeamEntity;
-//import main.java.repository.ProTeamRepo;
-//import main.java.service.ProTeamService;
-//
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(ProTeamService.class)
-//public class ProTeamServiceTest {
-//
-//	@Autowired
-//	private ProTeamRepo proTeamRepo;
-//
-//	@Autowired
-//	private ProTeamService proTeamService;	
-//
-//	ProTeamEntity proTeam = new ProTeamEntity();
-//	
-//	@Before
-//	public void setUp() {
-//		this.proTeamService = new ProTeamService(proTeamRepo); 
-//		proTeam.teamId = 10;
-//		proTeam.city = "CharlotteLight";
-//		proTeam.name = "HornetsTail";
-//		proTeam.mascot = "Giant Bee Thing";
-//	}
-//
-//	@Test
-//	public void get_all_teams_test() {
-//		assertTrue(ProTeamService.getTeams().size() == 3);
-//	}
-//
-//	@Test
-//	public void get_single_team_test() {
-//		assertTrue(ProTeamService.getSingleTeamAndRoster(1).name == "Hornets");
-//	}
-//
-//	@Test
-//	public void get_teams_by_field_lookup_test() {
-//		assertTrue(ProTeamService.getTeamsByFieldLookup(null, "Hickory", null).get(0).name == "Switch");
-//	}
-//
-//	@Test
-//	public void create_team_test() {
-//		ProTeamService.createTeam(proTeam);
-//		assertTrue(ProTeamService.getSingleTeamAndRoster(proTeam.teamId) != null);
-//	}
-//
-//	@Test
-//	public void update_team_test() {
-//		proTeam.mascot = "Monkey";
-//		assertTrue(ProTeamService.getSingleTeamAndRoster(proTeam.teamId).mascot != "Monkey");
-//		ProTeamService.updateTeam(proTeam.teamId, proTeam);
-//		assertTrue(ProTeamService.getSingleTeamAndRoster(proTeam.teamId).mascot == "Monkey");
-//	}
-//
-//	@Test
-//	public void delete_team_test() {
-//		assertTrue(ProTeamService.getSingleTeamAndRoster(proTeam.teamId) != null);
-//		ProTeamService.deleteTeam(proTeam.teamId);
-//		assertTrue(ProTeamService.getSingleTeamAndRoster(proTeam.teamId) == null);
-//	}
-//}
+package test.java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.entity.ProTeamEntity;
+import main.java.service.ProTeamService;
+
+public class ProTeamServiceTest {
+
+	@Before
+	public void setUp() {
+		ProTeamEntity team1 = new ProTeamEntity();
+		team1.teamId = 1;
+		team1.city = "Charlotte";
+		team1.name = "Hornets";
+		team1.mascot = "Giant Bee";
+		ProTeamEntity team2 = new ProTeamEntity();
+		team2.teamId = 2;
+		team2.city = "Hickory";
+		team2.name = "Switch";
+		team2.mascot = "A Stick";
+		ProTeamEntity team3 = new ProTeamEntity();
+		team3.teamId = 3;
+		team3.city = "Asheville";
+		team3.name = "Apps";
+		team3.mascot = "Mountain";
+	}
+
+	@Test
+	public void get_all_teams_test() {
+		assertTrue(ProTeamService.getTeams().size() == 3);
+	}
+
+	@Test
+	public void get_single_team_test() {
+		assertTrue(ProTeamService.getSingleTeamAndRoster(1).name == "Hornets");
+	}
+
+	@Test
+	public void get_teams_by_field_lookup_test() {
+		assertTrue(ProTeamService.getTeamsByFieldLookup(null, "Hickory", null).get(0).name == "Switch");
+	}
+
+	@Test
+	public void create_team_test() {
+		ProTeamEntity team4 = new ProTeamEntity();
+		team4.teamId = 14;
+		team4.city = "Morganton";
+		team4.name = "Shadows";
+		team4.mascot = "Dirt Roads";
+		ProTeamService.createTeam(team4);
+		assertTrue(ProTeamService.getSingleTeamAndRoster(team4.teamId) != null);
+	}
+
+	@Test
+	public void update_team_test() {
+		ProTeamEntity team1 = new ProTeamEntity();
+		team1.teamId = 1;
+		team1.city = "Space";
+		assertTrue(ProTeamService.getSingleTeamAndRoster(team1.teamId).city != "Space");
+		ProTeamService.updateTeam(team1.teamId, team1);
+		assertTrue(ProTeamService.getSingleTeamAndRoster(team1.teamId).city == "Space");
+	}
+
+	@Test
+	public void delete_team_test() {
+		assertTrue(ProTeamService.getSingleTeamAndRoster(1) != null);
+		ProTeamService.deleteTeam(1);
+		assertTrue(ProTeamService.getSingleTeamAndRoster(1) == null);
+	}
+}
